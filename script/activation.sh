31f9e8036dec034155ba8fa16438c3c238d95c5f
rere-rs <75220548+rere-rs@users.noreply.github.com>
2023-07-11 12:42:54 +0200

    Update activation.sh

--- script/activation.sh
+++ script/activation.sh
@@ -11,4 +11,4 @@ sudo xterm -hold -e sudo dnsmasq -d -C eviltwin/dnsmasq.conf &
 sudo xterm -hold -e sudo hostapd eviltwin/hostapd.conf &
 
 #Ouvre un terminal pour afficher les logs concernant la  capture des paquets
-sudo tshark -i $int_reseau -w eviltwin/sniff.cap &
+tshark -i $int_reseau -w eviltwin/sniff.cap &
