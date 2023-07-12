13eea27d72eb020ff28c10e60a96f2e6b632ade2
rere-rs <75220548+rere-rs@users.noreply.github.com>
2023-07-12 19:55:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo apt update
-
-pip install simple_term_menu psutil tqdm
-
-sudo apt install hostapd dnsmasq xterm -y
+sudo apt update #Necessaire sur un Kali Live
+pip install simple_term_menu psutil tqdm #Installe les modules python
+sudo apt install hostapd dnsmasq xterm -y #Installation hostapd dnsmasq et xterm
